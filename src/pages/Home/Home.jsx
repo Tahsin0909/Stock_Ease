253fa819c0dc72b1ad01b7314eacd6d27c6ecc22
Tahsin0909 <tajisan789@gmail.com>
2024-05-31 20:32:27 +0600

    Added toggle button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 
 const Home = () => {
 
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
+        <Box  padding='8px' rounded='full' boxShadow={colorMode === 'light' ?'xl' : 'dark-lg'} width='fit-content' height='fit-content'  onClick={toggleColorMode}>
+            {colorMode === 'light' ?
+            <img width='35px' height='35px' src="https://cdn-icons-png.flaticon.com/128/2698/2698194.png" alt="" />
+              : <img width='35px'height='35px' src="https://cdn-icons-png.flaticon.com/128/9689/9689800.png" alt="" />}
+            
+
+            {/* Toggle {colorMode === 'light' ? 'Dark' : 'Light'} */}
+        </Box >
     );
 };
 

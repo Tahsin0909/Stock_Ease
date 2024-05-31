f7e9620667eee13c2c094c2bd7e855aacd5a7c63
Tahsin0909 <tajisan789@gmail.com>
2024-05-31 19:30:01 +0600

    Added chakra ui theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { router } from './router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './Theme/Theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,14 @@
 import {
     createBrowserRouter
 } from "react-router-dom";
+import Home from "../pages/Home/Home";
 
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Home/> ,
     },
     {
         path: '/logIn',
